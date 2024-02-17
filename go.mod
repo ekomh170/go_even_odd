2a9978f49a155e99b462d4a7ca44bc21bd8dd827
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-02-17 14:20:07 +0700

    inisiasi file fungsi ganjil genap

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ekomh170/go_even_odd
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package go_even_odd
+
+func EvenOdd(n int) string {
+	if n%2 == 0 {
+		return "Even"
+	} else {
+		return "Odd"
+	}
+}
